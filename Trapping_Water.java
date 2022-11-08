1b5d0aacd696fed624a5dd4fc154af2dfd057786
Utkarsh Patel <108315201+utkarshhxd@users.noreply.github.com>
2022-11-08 15:48:25 +0530

    5th

--- Trapping_Water.java
+++ Trapping_Water.java
@@ -4,14 +4,14 @@ public class Trapping_Water {
 
     public static int trappedwater(int height[]) {
 
-        // for left max
+        // left max
         int leftMax[] = new int[height.length];
         leftMax[0] = height[0];
         for (int i = 1; i < height.length; i++) {
             leftMax[i] = Math.max(height[i], leftMax[i - 1]);
         }
 
-        // for right max
+        // right max
         int rightMax[] = new int[height.length];
         rightMax[height.length - 1] = height[height.length - 1];
         for (int i = height.length - 2; i >= 0; i--) {
